d7cd444e70accc2865a11e4687696fe70a36166d
Bill Wendling <morbo@google.com>
2016-07-17 01:45:51 -0700

    Fix some pylint warnings

--- yapftests/reformatter_test.py
+++ yapftests/reformatter_test.py
@@ -1605,7 +1605,8 @@ xxxxxxxxxxx, yyyyyyyyyyyy, vvvvvvvvv)
           style.CreateStyleFromConfig(
               '{based_on_style: pep8, indent_width: 2, '
               'continuation_indent_width: 4, indent_dictionary_value: True, '
-              'dedent_closing_brackets: True, split_before_named_assigns: False}'))
+              'dedent_closing_brackets: True, '
+              'split_before_named_assigns: False}'))
       code = textwrap.dedent("""\
           a_very_long_function_name(
               long_argument_name_1=1,
@@ -2145,7 +2146,8 @@ class BuganizerFixes(ReformatterTest):
     try:
       style.SetGlobalStyle(
           style.CreateStyleFromConfig(
-              '{based_on_style: chromium, SPLIT_BEFORE_LOGICAL_OPERATOR: True}'))
+              '{based_on_style: chromium, '
+              'SPLIT_BEFORE_LOGICAL_OPERATOR: True}'))
       code = textwrap.dedent("""\
           class foo():
 
