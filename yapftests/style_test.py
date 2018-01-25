b9e8f1f5800af9f45bbd84b66ba71b3e2b80978e
isabelacalinoiu <icalinoiu@google.com>
2018-01-25 11:36:04 +0100

    Update style_test.py

--- yapftests/style_test.py
+++ yapftests/style_test.py
@@ -225,13 +225,11 @@ class StyleFromDict(unittest.TestCase):
 
   def testDefaultBasedOnStyleBadDict(self):
     self.assertRaisesRegexp(style.StyleConfigError, 'Unknown style option',
-                            style.CreateStyleFromConfig, {
-                                'based_on_styl': 'pep8'
-                            })
+                            style.CreateStyleFromConfig,
+                            {'based_on_styl': 'pep8'})
     self.assertRaisesRegexp(style.StyleConfigError, 'not a valid',
-                            style.CreateStyleFromConfig, {
-                                'INDENT_WIDTH': 'FOUR'
-                            })
+                            style.CreateStyleFromConfig,
+                            {'INDENT_WIDTH': 'FOUR'})
 
 
 class StyleFromCommandLine(unittest.TestCase):
