5bc5a7b3e723e9b6b7555f659400a7ae106adb85
Bill Wendling <isanbard@gmail.com>
2021-12-24 01:24:05 -0600

    Fix up the "name" and "is_docstring" bits of a format_token

--- yapftests/format_token_test.py
+++ yapftests/format_token_test.py
@@ -67,13 +67,15 @@ class FormatTokenTest(unittest.TestCase):
 
   def testSimple(self):
     tok = format_token.FormatToken(pytree.Leaf(token.STRING, "'hello world'"))
-    self.assertEqual("FormatToken(name=STRING, value='hello world', lineno=0)",
-                     str(tok))
+    self.assertEqual(
+        "FormatToken(name=DOCSTRING, value='hello world', column=0, lineno=0)",
+        str(tok))
     self.assertTrue(tok.is_string)
 
     tok = format_token.FormatToken(pytree.Leaf(token.COMMENT, '# A comment'))
-    self.assertEqual('FormatToken(name=COMMENT, value=# A comment, lineno=0)',
-                     str(tok))
+    self.assertEqual(
+        'FormatToken(name=COMMENT, value=# A comment, column=0, lineno=0)',
+        str(tok))
     self.assertTrue(tok.is_comment)
 
   def testIsMultilineString(self):
