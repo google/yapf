9e1b23df898bf81435e2d395433de754d8e43361
Kyle Gottfried <Kyle.Gottfried@outlook.com>
2023-05-01 22:57:28 -0400

    pygram - remove unused imports

--- yapf/ylib2to3/pygram.py
+++ yapf/ylib2to3/pygram.py
@@ -6,9 +6,7 @@
 import os
 
 # Local imports
-from .pgen2 import token
 from .pgen2 import driver
-from . import pytree
 
 # The grammar file
 _GRAMMAR_FILE = os.path.join(os.path.dirname(__file__), "Grammar.txt")
