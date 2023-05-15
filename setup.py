909e149712b5bef1c764e039c569894873263e49
Kyle Gottfried <kyle.gottfried@outlook.com>
2023-05-15 16:20:16 -0400

    do not do fancy things with modules

--- setup.py
+++ setup.py
@@ -40,8 +40,7 @@ class RunTests(Command):
 
 
 PKG_INFO = ConfigParser()
-PKG_INFO_FILE = Path(
-    sys.modules[__name__].__file__).parent / 'yapf/PKG_INFO.ini'  # type: ignore
+PKG_INFO_FILE = Path('yapf/PKG_INFO.ini')  # type: ignore
 print(f'{Path(PKG_INFO_FILE)=}')
 print(f'Path exists?: {Path(PKG_INFO_FILE).exists()}')
 PKG_INFO.read(PKG_INFO_FILE)
