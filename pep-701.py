7bdb16e0fcb0f58b34bf16a9ad99f70adf463380
Kyle Gottfried <kyle.gottfried@outlook.com>
2023-09-14 10:31:41 -0400

    add test py

---
+++ pep-701.py
@@ -0,0 +1,14 @@
+bag = {'wand': 'unicorn'}
+
+print(f'Magic wand: { bag['wand'] }')
+
+things = ['wand', 'cloak']
+
+source = 'a_file.py'
+
+print(f'These are the things: {', '.join(things)}')
+print(f'{source.removesuffix('.py')}.c: $(srcdir)/{source}')
+print(f'{f'{f'infinite'}'}' + ' ' + f'{f'nesting!!!'}')
+
+a = ['hello', 'world']
+print(f"{'\n'.join(a)}")
