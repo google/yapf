901ff12405ee51a56c552d113b19288780bf6270
Andrea-Oliveri <andrea_oliveri@outlook.com>
2025-08-13 09:56:58 +0200

    Updated name of knob in style

--- yapf/yapflib/style.py
+++ yapf/yapflib/style.py
@@ -52,15 +52,56 @@ def SetGlobalStyle(style):
 
 
 _STYLE_HELP = dict(
-    ALIGN_CLOSING_BRACKET_WITH_VISUAL_INDENT=textwrap.dedent("""\
-      Align closing bracket with visual indentation."""),
     ALIGN_ASSIGNMENT=textwrap.dedent("""\
       Align assignment or augmented assignment operators.
       If there is a blank line or newline comment or objects with newline entries in between,
-      it will start new block alignment."""),
-    NEW_ALIGNMENT_AFTER_COMMENTLINE=textwrap.dedent("""\
-      Start new assignment or colon alignment when there is a newline comment in between."""
-                                                   ),
+      it will start new block alignment. For exemple:
+
+        val_first = 1
+        val_second += 2
+        object = {
+          entry1:1,
+          entry2:2,
+          entry3:3,
+          }
+        val_third = 3
+
+      will be formatted as: 
+
+        val_first   = 1
+        val_second += 2
+        object      = {
+            entry1: 1,
+            entry2: 2,
+            entry3: 3,
+        }
+        val_third = 3
+      """),
+    ALIGN_ASSIGNMENT_RESTART_AFTER_COMMENTS=textwrap.dedent("""\
+      Start new assignment alignment block when there is a newline comment in
+      between. For example if this is off then:
+      
+      val_first = 1
+      val_second += 2
+      # comment
+      val_third = 3
+
+      will be formatted as:
+
+      val_first   = 1
+      val_second += 2
+      # comment
+      val_third   = 3
+
+      and if it is on it will be formatted as:
+      
+      val_first   = 1
+      val_second += 2
+      # comment
+      val_third = 3
+      """),
+    ALIGN_CLOSING_BRACKET_WITH_VISUAL_INDENT=textwrap.dedent("""\
+      Align closing bracket with visual indentation."""),
     ALLOW_MULTILINE_LAMBDAS=textwrap.dedent("""\
       Allow lambdas to be formatted on more than one line."""),
     ALLOW_MULTILINE_DICTIONARY_KEYS=textwrap.dedent("""\
@@ -425,9 +466,9 @@ _STYLE_HELP = dict(
 def CreatePEP8Style():
   """Create the PEP8 formatting style."""
   return dict(
-      ALIGN_CLOSING_BRACKET_WITH_VISUAL_INDENT=True,
       ALIGN_ASSIGNMENT=False,
-      NEW_ALIGNMENT_AFTER_COMMENTLINE=False,
+      ALIGN_ASSIGNMENT_RESTART_AFTER_COMMENTS=False,
+      ALIGN_CLOSING_BRACKET_WITH_VISUAL_INDENT=True,
       ALLOW_MULTILINE_LAMBDAS=False,
       ALLOW_MULTILINE_DICTIONARY_KEYS=False,
       ALLOW_SPLIT_BEFORE_DEFAULT_OR_NAMED_ASSIGNS=True,
@@ -615,9 +656,9 @@ def _IntOrIntListConverter(s):
 #
 # Note: this dict has to map all the supported style options.
 _STYLE_OPTION_VALUE_CONVERTER = dict(
-    ALIGN_CLOSING_BRACKET_WITH_VISUAL_INDENT=_BoolConverter,
     ALIGN_ASSIGNMENT=_BoolConverter,
-    NEW_ALIGNMENT_AFTER_COMMENTLINE=_BoolConverter,
+    ALIGN_ASSIGNMENT_RESTART_AFTER_COMMENTS=_BoolConverter,
+    ALIGN_CLOSING_BRACKET_WITH_VISUAL_INDENT=_BoolConverter,
     ALLOW_MULTILINE_LAMBDAS=_BoolConverter,
     ALLOW_MULTILINE_DICTIONARY_KEYS=_BoolConverter,
     ALLOW_SPLIT_BEFORE_DEFAULT_OR_NAMED_ASSIGNS=_BoolConverter,
